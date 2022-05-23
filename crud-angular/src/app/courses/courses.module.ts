4f814f3e4999f5592aaa53862ff5030e1c60d2a4
Sandro Giacomozzi <sandrogiacom@gmail.com>
2022-05-23 20:09:01 -0300

    Add MatToolbarModule

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
-
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
+  declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
-  ]
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
 })
-export class CoursesModule { }
+export class CoursesModule {}
