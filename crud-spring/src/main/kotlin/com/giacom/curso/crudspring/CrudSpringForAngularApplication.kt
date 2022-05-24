fa36426e1cae2a17889bda8c7e9e95fb06c8ae01
Sandro Giacomozzi <sandrogiacom@gmail.com>
2022-05-24 04:43:06 -0300

    Add Spring project

---
+++ crud-spring/src/main/kotlin/com/giacom/curso/crudspring/CrudSpringForAngularApplication.kt
@@ -0,0 +1,11 @@
+package com.giacom.curso.crudspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudSpringForAngularApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudSpringForAngularApplication>(*args)
+}
